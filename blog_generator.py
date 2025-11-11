8873d34c9b8d269a5b598ebaf7d82a53cb4f5a15
Abhay_ <abhayislit@gmail.com>
2025-11-11 11:10:27 +0530

    Refactor blog_generator.py for improved API usage
    Updated API key handling and generation config parameters. Added system instruction for concise responses.

--- blog_generator.py
+++ blog_generator.py
@@ -1,12 +1,14 @@
 from google import genai
+from dotenv import dotenv_values
 
+config = dotenv_values(".env")
 
 # Setting up API key.
 
-client = genai.Client(api_key="")
+client = genai.Client()
 
 generation_config = genai.types.GenerationConfig(
-    max_output_tokens=67
+    max_output_tokens=50,
 )
 # ---- ----- ---- ---- ----- ----- ----- ----- ----- ----- ----- ------ 
 
@@ -18,11 +20,11 @@ work = input("Want me to write someting on any topic? (y/n) :     ")
 if work.lower() == 'y':
     prompt = input("What's on your mind? :")
     response = client.models.generate_content(
-    model="gemini-2.5-flash", contents= prompt
+    model="gemini-2.5-flash", 
+    contents= prompt+ "System instruction: Make the response consice and to the point. while keeeping it short and structured" 
     )
     print(response.text)
 else:
     print("Okay, see you later!")
-
 
 
